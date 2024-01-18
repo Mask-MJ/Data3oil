7ab39e4252021acee22c784d6cc29d17dd121652
Lizzie <820597849@qq.com>
2024-01-18 23:19:21 +0800

    feat(common): :sparkles: 新增联系我们

---
+++ src/components/common/Contact.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- 联系 -->
+  <div class="contact-bg w-full h-80 flex-center">
+    <div class="py-5 w-150 color-white">
+      <div class="text-4xl font-bold mb-10 text-center"> 和我们的专家聊聊您的项目？ </div>
+      <div class="mb-10 text-center">了解我们如何为专为您提供优质服务</div>
+      <div
+        class="w-50 h-15 border m-auto cursor-pointer border-white text-lg b-rd-1 flex-center text-center"
+      >
+        立即咨询
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .contact-bg {
+    background-image: url('/common/lianxi-bg.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
